fc6471558095701521f397fb6cd96d005d2e2087
Carter <carterplasek@gmail.com>
2018-09-16 16:07:45 -0500

    Random Prime Generator

---
+++ Code/primes.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2,int(num**0.5)+2):
+        if i >= num:
+            return True
+        if num/float(i) == int(num/i):
+            return False
+    return True
+
+def getRandomPrime(low, high=None):
+    if high == None:
+        high = low
+        low = 2
+
+    p = 1
+
+    while not isPrime(p):
+        p = random.randint(low, high)
+
+    return p
