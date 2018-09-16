b0e3a6640a163cff7c5adb30e769e8fe5d06ef5f
Carter <carterplasek@gmail.com>
2018-09-16 15:51:56 -0500

    Finalized Key Exchange Class

--- Code/keyExchange.py
+++ Code/keyExchange.py
@@ -1,10 +1,15 @@
+import random
+
 class KeyExchange:
     def __init__(self,primes):
         self.p, self.g = primes
         self.secret = 0
 
-    def setSecred(self,secret):
+    def setSecret(self,secret):
         self.secret = secret
+
+    def randomSecret(self):
+        self.secret = random.randint(2,50)
 
     def calculateMixed(self):
         return self.g ** self.secret % self.p
--- Code/main.py
+++ Code/main.py
@@ -2,3 +2,15 @@ import socket
 import threading
 
 import keyExchange
+
+alice = keyExchange.KeyExchange((337, 11))
+bob = keyExchange.KeyExchange((337, 11))
+
+alice.randomSecret()
+bob.randomSecret()
+
+A = alice.calculateMixed()
+B = bob.calculateMixed()
+
+print (alice.getSharedKey(B))
+print (bob.getSharedKey(A))
