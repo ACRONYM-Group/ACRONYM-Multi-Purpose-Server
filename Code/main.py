8e2af69a8dc1cb93f34bca56f872dc1b47d2ac83
Carter <carterplasek@gmail.com>
2018-09-16 16:35:22 -0500

    Starting the Networking Code

--- Code/main.py
+++ Code/main.py
@@ -5,7 +5,22 @@ import keyExchange
 
 import primes as Primes
 
-serverSocket = socket.socekt(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 4242
 hostName = ""
+
+serverSocket = socket.bind((hostName, port))
+
+def connectionHandler(conn, addr):
+    print ("Connection Recieved From " + str(addr))
+
+def listener(sock):
+    while True:
+        sock.listen(1)
+        conn, addr = sock.accept()
+
+        threading.Thread(target=connectionHandler, args=(conn,addr)).start()
+
+def startListener(sock):
+    threading.Thread(target=listener, args=(sock,)).start()
