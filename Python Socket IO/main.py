a72b55cbb9d9e3c5d8e7d77f94f502eca362614c
Carter <carterplasek@gmail.com>
2018-09-17 19:20:14 -0500

    Request Relay System

--- Python Socket IO/main.py
+++ Python Socket IO/main.py
@@ -1,7 +1,35 @@
 import socket
+import threading
 
 hostingSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = ""
 port = 80
 
+hostingSocket.bind((host, port))
+
+def connectionHandler(conn, addr):
+    print ("Got Connection From: " + str(addr[0])))
+
+    data = conn.recv(4096)
+
+    while data != "":
+        print (data)
+
+        data = conn.recv(4096)
+
+    conn.close()
+
+    print ("Connection with " + str(addr[0]) + " closed")
+
+def listener(sock):
+    while True:
+        sock.listen(1)
+        conn, addr = sock.accept()
+
+        threading.Thread(target=connectionHandler, args=(conn, addr)).start()
+
+def startListener(sock):
+    threading.Thread(target=listener, args = (sock,)).start()
+
+startListener(hostingSocket)
