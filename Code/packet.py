8797c77bc9c3f3e428006d87f1b147ec23d7d6f3
CarterTS <carterplasek@gmail.com>
2018-12-29 15:45:41 -0600

    PEP8-ify packet.py

--- Code/packet.py
+++ Code/packet.py
@@ -7,25 +7,24 @@ __HDS__     Handshake
 __DAT__     Raw Data (Binary)
 __LPW__     Large Packet Wrapper
 """
-
 import json
 import math
 import time
-import struct
+
 
 class Packet:
-    def __init__(self, body, packetType="__RAW__", connection = None):
+    def __init__(self, body, packetType="__RAW__", connection=None):
         self.connection = connection
 
         self.body = body
         self.type = packetType
 
-    def send(self, conn=None, windowID = None):
+    def send(self, conn=None, windowID=None):
         LPWID = time.clock()
-        if conn == None:
+        if conn is None:
             conn = self.connection
 
-        data = {"packetType": self.type, "payload":self.body}
+        data = {"packetType": self.type, "payload": self.body}
 
         data = json.dumps(data)
         LPWPacketLength = 2048
@@ -39,28 +38,32 @@ class Packet:
             currentDataIndex = 0
             while currentDataIndex < len(data):
                 i = i + 1
-                LPWPayload = data[currentDataIndex:currentDataIndex + LPWPacketLength]
-                LargePacketWrapper = {"packetType":"__LPW__", "LPWID": LPWID, "len": numberOfLPWPackets, "ind": i, "windowID":windowID, "payload":LPWPayload}
+                LPWPayload = data[currentDataIndex:currentDataIndex +
+                                  LPWPacketLength]
+                LargePacketWrapper = {"packetType": "__LPW__",
+                                      "LPWID": LPWID,
+                                      "len": numberOfLPWPackets,
+                                      "ind": i,
+                                      "windowID": windowID,
+                                      "payload": LPWPayload}
                 currentDataIndex = currentDataIndex + LPWPacketLength
                 dataToSend = json.dumps(LargePacketWrapper).encode()
                 dataToSend = dataToSend + "-ENDACROFTPPACKET-/".encode()
                 conn.sendall(dataToSend)
 
-        
-
     def __repr__(self):
-        return "self.body = " + '"' + str(self.body) + '"\n' + "self.type = " + '"' + str(self.type) + '"'
+        return("self.body = " +
+               '"' + str(self.body) +
+               '"\n' + "self.type = " +
+               '"' + str(self.type) + '"')
+
 
 def readPacket(connection):
     data = connection.recv(4096000)
     try:
         data = data.decode()
-    except:
+    except Exception:
         data2 = connection.recv(4096000)
         data += data2
         data = data.decode()
     return data
-
-
-
-    
