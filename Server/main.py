fb891389dcee923293528e6f2c74e60cbe1bbfad
Carter <carterplasek@gmail.com>
2019-02-01 22:23:15 -0600

    More Path stuff

--- Server/main.py
+++ Server/main.py
@@ -36,7 +36,7 @@ OSName = platform.platform()
 
 programInstallDirectory = "Z:/AcroFTP/"
 
-AccountHandler.DATA_FILE_PATH = programInstallDirectory + "data.json"
+AccountHandler.DATA_FILE_PATH = programInstallDirectory + "\\data\\data.json"
 
 AccountHandler.import_data()
 
